d7dd9a1284f45be6a2e1867171e49cd38aa347ff
inGoodM <a.kholodovskyi@gmail.com>
2023-03-20 19:02:06 +0200

    Initial Commit

---
+++ The weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The weather
+//
+//  Created by InGMood on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
